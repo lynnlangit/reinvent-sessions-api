d8c1da9f2c18d92034be21d6124756151a7688a4
ryo nakamaru <pottava@gmail.com>
2015-08-26 13:57:04 +0900

    adjust Dockers 'automated build'

--- vagrant/scripts/coreos-init.sh
+++ vagrant/scripts/coreos-init.sh
@@ -3,5 +3,5 @@
 systemctl stop ap
 systemctl stop db
 
-docker build -f /home/core/share/docker/Dockerfile.base -t supinf/reinvent-sessions:base .
+docker pull supinf/reinvent-sessions-api:base
 docker pull deangiberson/aws-dynamodb-local
