dba6a775f601024be328f98a8165da179a6bffaf
ryo nakamaru <pottava@gmail.com>
2015-08-28 16:44:38 +0900

    add a Dockerfile for production mode & monitoring container

--- vagrant/scripts/coreos-init.sh
+++ vagrant/scripts/coreos-init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+systemctl stop monit
 systemctl stop ap
 systemctl stop db
 
-docker pull supinf/reinvent-sessions-api:base
+docker build -f /home/core/share/docker/Dockerfile.base -t supinf/reinvent-sessions-api:base .
 docker pull deangiberson/aws-dynamodb-local
+docker pull pottava/docker-webui:latest
--- vagrant/scripts/coreos-up.sh
+++ vagrant/scripts/coreos-up.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+systemctl stop monit
 systemctl stop ap
 systemctl stop db
 
 systemctl start db
 systemctl start ap
+systemctl start monit
